a5c9504614713cc35e9249ce965c077e6682364b
david <dck003@ucsd.edu>
2024-04-04 13:56:40 -0700

    Added ascii+animation

--- exercise-1-main.cpp
+++ exercise-1-main.cpp
@@ -13,44 +13,114 @@ void clearScreen(){
     system("CLS");
   #endif
 }
+void eveLeftToRight(int start, int end){
+  string eveRight[] ={"           @@#@@@@.    \n",
+  "        @           #  \n",
+  "       @         @@@@@ \n",
+  "      @       @@@@@@@@@\n",
+  "      @      @@@@@@@.-@\n",
+  "      :     @@@@..:@@@@\n",
+  "     @@@    @@@@@@@@@  \n",
+  "      #@%-*@     -.@@  \n",
+  "         .@@@@@@@@@+.. \n",
+  "   @@@@+ @          @  \n",
+  "  @    @ @         .@# \n",
+  " @    -@ @         @ @ \n",
+  " =    @- @        @. @ \n",
+  "@    @*+:@       .@  @ \n",
+  "@   @ + .@       +  @  \n",
+  "@  @   @@*      @ @@   \n",
+  " @@     #@:***@+       \n"};
+  string padding = "";
+  for(int i=start; i < end; i++, padding=padding+" ")
+  {
+    for(string line: eveRight)
+    {
+      cout << padding << line;
+    }
+    usleep(5000);
+    clearScreen();
+  }
+}
+
+void eveRightToLeft(int start, int end){
+  string eveLeft[] = {"    %@@@@#@@            \n",
+  "  -%          @         \n",
+  " @@@@@         @        \n",
+  "@@@@@@@@@       @       \n",
+  "@-.@@@@@@@      %       \n",
+  "@@@@:..%@@@     .       \n",
+  "  @@@@@@@@@    @@@      \n",
+  "  @@.=     @*=*@% @     \n",
+  "  ==@@@@@@@@@:    :     \n",
+  "  @          % =@@@@    \n",
+  " +@:         @ @    @   \n",
+  " @ @         @.@#    @  \n",
+  " @  @        @.-@    .: \n",
+  " #  @        @:+*@    @ \n",
+  "  @  :       @: * @   % \n",
+  "   @@ @      +@@   @  @ \n",
+  "       :@***:@%     @@  \n"};
+  string padding(end, ' ');
+  for(int i=end; i>start; i--, padding.erase(0,1)){
+    for(string line: eveLeft)
+    {
+      cout << padding << line;
+    }
+    usleep(5000);
+    clearScreen();
+  }
+
+}
+
 int main(){
     string ans;
     //Eve to WALLE
-    cout << "Can we end future suffering: ";
-    //trigger python animation here
-    getline(cin, ans);
-    while(ans != "no"){
-      cout << "Can we end future suffering: ";
-      getline(cin, ans);
-    }
-    //trigger python animation here
-    cout << "\nSo are humans doomed to suffer?\n";
-    sleep(3);
-    clearScreen();
-    cout << "Consider how many ways we can end future suffering.\n";
-    sleep(3); 
-    clearScreen();
-    cout << "Computing solutions to the suffering problem: ";
-    getline(cin, ans);
-    //cout << atoi(ans.c_str()) << "\n" ;
-    cout << "> 1 solutions found. Stored in answer_var.\n";
-    sleep(3);
-    clearScreen();
-    cout << "There is no single answer to the solution.\n";
-    sleep(3);
-    clearScreen();
-    cout << "Recomputing solutions with parameters: answer_var != 1 && answer_var > 1.\n";
-    sleep(3);
-    clearScreen();
-    cout << "Theres thousands of answers, you can be the answer.\n";
-    sleep(3);
-    clearScreen();
+    string walle="                                            .          .                                            \n                                        @@@%  @@    @@  #@@@                                        \n                                    @@@@:   @  @    @  @   .@@@@                                    \n                               #@@@@*  :@@@   =@%@@%@*   @@@-  +@@@@%                               \n                           @@@@@@          @@ @@    @@ @@          @@@@@@                           \n                         @@   =*   @@@@@@   @@@@    @@@@   @@@@@@   +*   @@.                        \n                       +@    -@   @@    @@   %@      @@   @@    @@   @*    @#                       \n                      .@  @@ =@   @@    #@   @@ #@@% @@   @@    @@   @# @@  @=                      \n                      @%      @-   @@@@@@   @@        @@   @@@@@@   .@      #@                      \n                      @=      .@%          %@@@@@@@@@@@@@          #@-      :@                      \n                      *@        @@@      @@@.           @@@      @@@        @#                      \n                       @=          @@@@@#@@@            @@@#@@@@@          :@.                      \n                        @@      @@      %.  @@@@@@@@@@@@  .@      @@      @@.                       \n                         @@@+       +@@@@# =@%@      @%@+ *@@@@#       -@@@                         \n                            @@@@@@@@@     +. @@      @@ .+     @@@@@@@@@                            \n                                             %@      @@                                             \n                                            @@@@@@@@@@@@                                            \n                                           @@          @@                                           \n                                           @@@@@@@@@@@@@@                                           \n                                           @@          @@                                           \n                                           @@          @@                                           \n                                           %@          @@                                           \n                         =@@@@@@@@@@@@@@@@@@@%@@@@@@@@%@@@@@@@@@@@@@@@@@@@*                         \n                      .@@@         %@.                         @@         #@@.                      \n                    @@@=@           @       ....               @:          @*@@@                    \n                   :@   @@@@@@@@@@@@@   @@-+###*.@@    @@@:    @@@@@@@@@@@@@   @+                   \n                    @   @           @   @@       @@   @@  @@   @=          @   @                    \n                  -@@@@@@@@@@@@@-   @   @@       @@    @@@@    @=  :@@@@@@@@@@@@@+                  \n                 @:        @    #@@@@@@ %@@@@@@@@@:          @@@@@@%    @.        @                 \n                @@@@@      @         :@                      @%         @:     @@@@@                \n                @   #%     @         .@@@@@@@@@@@@@@@@@@@@@@@@#         @     +%   @                \n                @   +@@@@@@@@@@@@@@@@@@                      @@@@@@@@@@@@@@@@@@#   @                \n                @.  +:             %                            @              #   @                \n                @   +@@@@@@@@@@@@@@@@@@                      @@@@@@@@@@@@@@@@@@#   @                \n                @   *#     @         .@                      @#         @     +%   @                \n                @@@@@.    .@         :@                      @%         @:     @@@@@                \n                 @.        @    *@@@@@@                      @@@@@@#    @         @                 \n                  -@@@@@@@@@@@@@+                                  +@@@@@@@@@@@@@*                  \n                        @                                                  @                        \n @@#+*************%@@:  @                                                  @  .@@@*************+#@@ \n *                  *-  @                                                  @  .*                  # \n.@                  +@@@@                                                  @@@@*                  @:\n-@                  -*  @                                                  @  =+                  @-\n *@@@@@@@@@@@@@@@@@@*+  @                                                  @  =*@@@@@@@@@@@@@@@@@@* \n-@                  -@  @                                                  @  %=                  @-\n *.@@@@@@@@@@@@@@@@#**  @                                                  @  =*#@@@@@@@@@@@@@@@@.* \n:@                  +%  @                                                  @  *#                  @-\n-@                  =@   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.  @+                  @-\n * %%%%%%%%%%%%%%%#=**@@          @                              @          @@#*+#%%%%%%%%%%%%%%% * \n-@                  --  @@@@@@@@@@@                              @@@@@@@@@@@. -=                  @-\n * %%%%%%%%%%%%%%%+:**  @         @                              @         @. +*:+%%%%%%%%%%%%%%% # \n-@                  =@  @     =   @                              @   *     @. %*                  @-\n:@                  =@.-@       *@@                              @@@       @+.@*                  @-\n % +++++++++++++++: ** :@@@@@@@@@                                  @@@@@@@@@+ +* .+++++++++++++++ @ \n-@                  -%                                                        *+                  @-\n * %@@@@@@@@@@@@@@#=*+                                                        =*=#@@@@@@@@@@@@@@% * \n *                  -:                                                        .-                  + ";
+    string eve="                                                                                                    \n                                                                                                    \n                                                                                                    \n                                                          @@%@**.+@                                 \n                                                       @@:  -.       +                              \n                                                     .    :. .                                      \n                                                    +@@@@@+             -                           \n                                                   @@@@@@@@@@.           .                          \n                                                  *@%@@@@@@@@@@          .                          \n                                                  @@--@@@@@@@@@@@         *                         \n                                                  @@@@@@@---+@@@@@        @                         \n                                                   @@@@@@+--@@@@@@                                  \n                                                     @@@@@@@@@@@@@       @@@                        \n                                                      #-*@@@@@@     .@#%.@@%@                       \n                                                     @*:.=      .+%   -@@   @                       \n                                                    +#.@@@@@%**@@@@@@+      @                       \n                                                                       =*@@@@=                      \n                                                     @. ...  ...         :    @.                    \n                                                    +#=  .......      @ @      @:                   \n                                                    + @  .......      @.@@      @                   \n                                                    @  @  ......      @  @=      @                  \n                                                    @  .* ......      @ . @       @                 \n                                                    #   @  ......     @ -. @      %                 \n                                                    #= =:%  :.....   .@ = @ @      @                \n                                                     @ -  @  ......  .@. =   @     *                \n                                                      %@=  @  ...... .@  @    @    @                \n                                                       .    @   ...:.  @@     @:  @                 \n                                                              @. ..  #@         :#                  \n                                                                @.  %                               \n";
+    string eveLeft="    %@@@@#@@            \n  -%          @         \n @@@@@         @        \n@@@@@@@@@       @       \n@-.@@@@@@@      %       \n@@@@:..%@@@     .       \n  @@@@@@@@@    @@@      \n  @@.=     @*=*@% @     \n  ==@@@@@@@@@:    :     \n  @          % =@@@@    \n +@:         @ @    @   \n @ @         @.@#    @  \n @  @        @.-@    .: \n #  @        @:+*@    @ \n  @  :       @: * @   % \n   @@ @      +@@   @  @ \n       :@***:@%     @@  ";
+    // clearScreen();
+    // cout << walle;
+    // sleep(4);
+
+    // cout << "\nCan we end future suffering?: ";
+
+    // getline(cin, ans);
+    // while(ans != "no"){
+    //   cout << "\nCan we end future suffering: ";
+    //   getline(cin, ans);
+    // }
+    // clearScreen();
+    // cout << eve << "\nSo are humans doomed to suffer?\n";
+    // sleep(3);
+    // clearScreen();
+    // cout << "Consider how many ways we can end future suffering.\n" << walle;
+    // sleep(3); 
+    // clearScreen();
+    // cout << eve << "Computing solutions to the suffering problem: ";
+    // getline(cin, ans);
+    // //cout << atoi(ans.c_str()) << "\n" ;
+    // clearScreen();
+    // cout << eve << "num_of_solutions > 1 Stored in answer_var.\n";
+    // sleep(3);
+    // clearScreen();
+    // cout << "There is no single answer to the solution.\n" << walle;
+    // sleep(3);
+    // clearScreen();
+    // cout << "\nRecomputing solutions with parameters: num_of_solutions != 1 && num_of_solutions > 1.\n" << eve;
+    // sleep(3);
+    // clearScreen();
+    // cout << "There could be thousands of answers, and you can be one of them.\n" << eve;
+    // sleep(3);
+    // clearScreen();
 
 
-    cout << "added parameter, answer_var >= 1000\n";
-    cout << "Affirmative, [this.self] executing process \"answer_var\".\n";
-    sleep(3);
-    clearScreen();
+    // cout << "added parameter, num_of_solutions >= 1000\n";
+    // cout << "Affirmative, [this.self] executing process \"become solution\".\n" << eve;
+    // sleep(3);
+    // clearScreen();
+    eveLeftToRight(0,170);
+    eveRightToLeft(0,170);
     cout << "THE END\n";
     //WALLE and eve?
     //cleaning animation (build up blocks)
